44af073a879ec1ad91db02294a9bc752a0e6de00
notageek-guy <manishkumar97430@gmail.com>
2024-04-14 18:02:20 +0530

    Added Compiler Api

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mnsdojo/gorun_api
+
+go 1.21.8
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type CompileRequest struct {
+	Code string `json:"code"`
+}
+
+type CompileResult struct {
+	Result string `json:"result"`
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	r.Post("/compile", compileHandler)
+
+	http.ListenAndServe(":3000", r)
+}
+
+func compileHandler(w http.ResponseWriter, r *http.Request) {
+	var req CompileRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Failed to parse JSON request body", http.StatusBadRequest)
+		return
+	}
+
+	// Write code to a temporary file
+	tempFile, err := os.CreateTemp("", "source*.go")
+	if err != nil {
+		http.Error(w, "Failed to create temporary file", http.StatusInternalServerError)
+		return
+	}
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	if _, err := tempFile.WriteString(req.Code); err != nil {
+		http.Error(w, "Failed to write code to temporary file", http.StatusInternalServerError)
+		return
+	}
+
+	output, err := compileCode(tempFile.Name())
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Compilation failed: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Return compilation output as JSON response
+	res := CompileResult{Result: output}
+	json.NewEncoder(w).Encode(res)
+}
+
+func compileCode(filePath string) (string, error) {
+	dir := filepath.Dir(filePath)
+
+	cmd := exec.Command("go", "run", filePath)
+	cmd.Dir = dir
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
